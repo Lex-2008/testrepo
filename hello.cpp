8c747eb4f803a0bc996300bbcd9ced66ecf9949c
Alexey Shpakovsky <alexey@shpakovsky.ru>
2026-01-28 11:55:05 +0100

    hello test

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+// "Hello, world!" program, (c) Bjarne Stroustrup
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!\n";
+}
--- port/portfile.cmake
+++
@@ -1,5 +0,0 @@
-vcpkg_cmake_configure(
-    SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}/test
-    OPTIONS
-        -DVCPKG_HOST_TRIPLET="${HOST_TRIPLET}"
-)
